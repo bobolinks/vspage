0cc9d6814288daa735fea746c241422b88ef1e10
amos <amos@amosdeMacBook-Pro.local>
2022-06-16 19:29:31 +0800

    删除多余文件

--- service/types/index.d.ts
+++ service/types/index.d.ts
@@ -1,6 +1,3 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path='./types.d.ts' />
-/// <reference path='./facade.d.ts' />
-/// <reference path='./project.d.ts' />
-/// <reference path='./uikit.d.ts' />
 /// <reference path='./vspage.d.ts' />
